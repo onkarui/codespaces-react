f070a84f3f2ad7ebe8d6376e1657f641c04318fd
onkarui <128511396+onkarui@users.noreply.github.com>
2023-03-22 11:24:54 +0000

    update React Table

--- src/Table.js
+++ src/Table.js
@@ -1,71 +1,39 @@
 
 import React, { useState, useEffect } from 'react';
-import { classNames } from 'primereact/utils';
 import { FilterMatchMode, FilterOperator } from 'primereact/api';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { InputText } from 'primereact/inputtext';
-import { Dropdown } from 'primereact/dropdown';
-import { MultiSelect } from 'primereact/multiselect';
-import { Tag } from 'primereact/tag';
-import { TriStateCheckbox } from 'primereact/tristatecheckbox';
-import  {getCustomersMedium} from './service/CustomerService';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';   // theme
-import 'primereact/resources/primereact.css';                       // core css
-import 'primeicons/primeicons.css';                                 // icons
-// import 'primeflex/primeflex.css';                                   // css utility
-
+import { getCustomersMedium } from './service/CustomerService';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeicons/primeicons.css';
 export default function BasicFilterDemo() {
     const [customers, setCustomers] = useState(null);
     const [filters, setFilters] = useState({
         global: { value: null, matchMode: FilterMatchMode.CONTAINS },
         firstName: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
         'lastName': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
-        representative: { value: null, matchMode: FilterMatchMode.IN },
-        status: { value: null, matchMode: FilterMatchMode.EQUALS },
-        verified: { value: null, matchMode: FilterMatchMode.EQUALS },
+        currentAddress: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        phoneNumber: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        mailingAddress: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        policyNumber: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        subscribeId: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        doNotEmail: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        doNotEmailCode: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        doNotCall: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        doNotCallCode: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        doNotMail: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        doNotMailCode: { value: null, matchMode: FilterMatchMode.CONTAINS },
     });
     const [loading, setLoading] = useState(true);
-    const [globalFilterValue, setGlobalFilterValue] = useState('');
-    const [representatives] = useState([
-        { name: 'Amy Elsner', image: 'amyelsner.png' },
-        { name: 'Anna Fali', image: 'annafali.png' },
-        { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
-        { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
-        { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
-        { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
-        { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
-        { name: 'Onyama Limba', image: 'onyamalimba.png' },
-        { name: 'Stephen Shaw', image: 'stephenshaw.png' },
-        { name: 'XuXue Feng', image: 'xuxuefeng.png' }
-    ]);
-    const [statuses] = useState(['unqualified', 'qualified', 'new', 'negotiation', 'renewal']);
-
-    const getSeverity = (status) => {
-        switch (status) {
-            case 'unqualified':
-                return 'danger';
-
-            case 'qualified':
-                return 'success';
-
-            case 'new':
-                return 'info';
-
-            case 'negotiation':
-                return 'warning';
-
-            case 'renewal':
-                return null;
-        }
-    };
 
     useEffect(() => {
         getCustomersMedium().then((data) => {
             setCustomers(getCustomers(data));
             setLoading(false);
         });
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    }, []); 
 
     const getCustomers = (data) => {
         return [...(data || [])].map((d) => {
@@ -73,27 +41,6 @@ export default function BasicFilterDemo() {
 
             return d;
         });
-    };
-
-    const onGlobalFilterChange = (e) => {
-        const value = e.target.value;
-        let _filters = { ...filters };
-
-        _filters['global'].value = value;
-
-        setFilters(_filters);
-        setGlobalFilterValue(value);
-    };
-
-    const renderHeader = () => {
-        return (
-            <div className="flex justify-content-end">
-                <span className="p-input-icon-left">
-                    <i className="pi pi-search" />
-                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
-                </span>
-            </div>
-        );
     };
 
     const countryBodyTemplate = (rowData) => {
@@ -112,80 +59,105 @@ export default function BasicFilterDemo() {
         );
     };
 
-    const representativeBodyTemplate = (rowData) => {
-        const representative = rowData.representative;
-
+    const phoneNumberTemplate = (rowData) => {
         return (
             <div className="flex align-items-center gap-2">
-                <img alt={representative.name} src={`https://primefaces.org/cdn/primereact/images/avatar/${representative.image}`} width="32" />
-                <span>{representative.name}</span>
+                <span>{rowData.phoneNumber}</span>
             </div>
         );
     };
 
-    const representativesItemTemplate = (option) => {
+    const mailingAddressTemplate = (rowData) => {
         return (
             <div className="flex align-items-center gap-2">
-                <img alt={option.name} src={`https://primefaces.org/cdn/primereact/images/avatar/${option.image}`} width="32" />
-                <span>{option.name}</span>
+                <span>{rowData.mailingAddress}</span>
             </div>
         );
     };
 
-    const statusBodyTemplate = (rowData) => {
-        return <Tag value={rowData.status} severity={getSeverity(rowData.status)} />;
-    };
-
-    const statusItemTemplate = (option) => {
-        return <Tag value={option} severity={getSeverity(option)} />;
-    };
-
-    const verifiedBodyTemplate = (rowData) => {
-        return <i className={classNames('pi', { 'true-icon pi-check-circle': rowData.verified, 'false-icon pi-times-circle': !rowData.verified })}></i>;
-    };
-
-    const representativeRowFilterTemplate = (options) => {
+    const policyNumberTemplate = (rowData) => {
         return (
-            <MultiSelect
-                value={options.value}
-                options={representatives}
-                itemTemplate={representativesItemTemplate}
-                onChange={(e) => options.filterApplyCallback(e.value)}
-                optionLabel="name"
-                placeholder="Any"
-                className="p-column-filter"
-                maxSelectedLabels={1}
-                style={{ minWidth: '14rem' }}
-            />
+            <div className="flex align-items-center gap-2">
+                <span>{rowData.policyNumber}</span>
+            </div>
         );
     };
 
-    const statusRowFilterTemplate = (options) => {
+    const subscribeIdTemplate = (rowData) => {
         return (
-            <Dropdown value={options.value} options={statuses} onChange={(e) => options.filterApplyCallback(e.value)} itemTemplate={statusItemTemplate} placeholder="Select One" className="p-column-filter" showClear style={{ minWidth: '12rem' }} />
+            <div className="flex align-items-center gap-2">
+                <span>{rowData.subscribeId}</span>
+            </div>
         );
     };
 
-    const verifiedRowFilterTemplate = (options) => {
-        return <TriStateCheckbox value={options.value} onChange={(e) => options.filterApplyCallback(e.value)} />;
+    const DoNotEmailTemplate = (rowData) => {
+        return (
+            <div className="flex align-items-center gap-2">
+                <span>{rowData.doNotEmail}</span>
+            </div>
+        );
     };
 
-    const header = renderHeader();
+    const DoNotEmailCodeTemplate = (rowData) => {
+        return (
+            <div className="flex align-items-center gap-2">
+                <span>{rowData.doNotEmailCode}</span>
+            </div>
+        );
+    };
+
+    const doNotCallTemplate = (rowData) => {
+        return (
+            <div className="flex align-items-center gap-2">
+                <span>{rowData.doNotCall}</span>
+            </div>
+        );
+    };
+
+    const doNotCallCodeTemplate = (rowData) => {
+        return (
+            <div className="flex align-items-center gap-2">
+                <span>{rowData.doNotCallCode}</span>
+            </div>
+        );
+    };
+
+    const doNotMailTemplate = (rowData) => {
+        return (
+            <div className="flex align-items-center gap-2">
+                <span>{rowData.doNotMail}</span>
+            </div>
+        );
+    };
+
+    const doNotMailCodeTemplate = (rowData) => {
+        return (
+            <div className="flex align-items-center gap-2">
+                <span>{rowData.doNotMailCode}</span>
+            </div>
+        );
+    };
 
     return (
         <div className="card">
             <DataTable value={customers} paginator rows={10} dataKey="id" filters={filters} filterDisplay="row" loading={loading}
-                    globalFilterFields={['firstName', 'lastName', 'representative.name', 'status']} header={header} emptyMessage="No customers found.">
+                  emptyMessage="No customers found.">
                 <Column field="firstName" header="firstName" filter filterPlaceholder="Search by name" style={{ minWidth: '12rem' }} />
                 <Column header="lastName" filterField="lastName" style={{ minWidth: '12rem' }} body={countryBodyTemplate} filter filterPlaceholder="Search by lastName" />
                 <Column header="currentAddress" filterField="currentAddress" style={{ minWidth: '12rem' }} body={addressBodyTemplate} filter filterPlaceholder="Search by currentAddress" />
+                <Column header="phoneNumber" filterField="phoneNumber" style={{ minWidth: '12rem' }} body={phoneNumberTemplate} filter filterPlaceholder="Search by phoneNumber" />
+                <Column header="mailingAddress" filterField="mailingAddress" style={{ minWidth: '12rem' }} body={mailingAddressTemplate} filter filterPlaceholder="Search by mailingAddress" />
+                <Column header="policyNumber" filterField="policyNumber" style={{ minWidth: '12rem' }} body={policyNumberTemplate} filter filterPlaceholder="Search by policyNumber" />
+                <Column header="subscribeId" filterField="subscribeId" style={{ minWidth: '12rem' }} body={subscribeIdTemplate} filter filterPlaceholder="Search by subscribeId" />
+                <Column header="doNotEmail" filterField="doNotEmail" style={{ minWidth: '12rem' }} body={DoNotEmailTemplate} filter filterPlaceholder="Search by DoNotEmail" />
+                <Column header="doNotEmailCode" filterField="doNotEmailCode" style={{ minWidth: '12rem' }} body={DoNotEmailCodeTemplate} filter filterPlaceholder="Search by DoNotEmailCode" />
+                <Column header="doNotCall" filterField="doNotCall" style={{ minWidth: '12rem' }} body={doNotCallTemplate} filter filterPlaceholder="Search by doNotCall" />
+                <Column header="doNotCallCode" filterField="doNotCallCode" style={{ minWidth: '12rem' }} body={doNotCallCodeTemplate} filter filterPlaceholder="Search by doNotCallCode" />
+                <Column header="doNotMail" filterField="doNotMail" style={{ minWidth: '12rem' }} body={doNotMailTemplate} filter filterPlaceholder="Search by doNotMail" />
+                <Column header="doNotMailCode" filterField="doNotMailCode" style={{ minWidth: '12rem' }} body={doNotMailCodeTemplate} filter filterPlaceholder="Search by doNotMailCode" />
 
-                <Column header="Agent" filterField="representative" showFilterMenu={false} filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '14rem' }}
-                    body={representativeBodyTemplate} filter filterElement={representativeRowFilterTemplate} />
-                <Column field="status" header="Status" showFilterMenu={false} filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '12rem' }} body={statusBodyTemplate} filter filterElement={statusRowFilterTemplate} />
-                <Column field="verified" header="Verified" dataType="boolean" style={{ minWidth: '6rem' }} body={verifiedBodyTemplate} filter filterElement={verifiedRowFilterTemplate} />
             </DataTable>
         </div>
     );
 }
-        
